d9775451425c840e76a7326e6daf417f4d3eaddb
Steve DiGirolamo <steve@arizonawebdevelopment.com>
2018-06-06 21:49:23 -0700

    initial commit

---
+++ wp-config.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the
+ * installation. You don't have to use the web site, you can
+ * copy this file to "wp-config.php" and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * MySQL settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://codex.wordpress.org/Editing_wp-config.php
+ *
+ * @package WordPress
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define('DB_NAME', 'pursuit');
+
+/** MySQL database username */
+define('DB_USER', 'root');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'root');
+
+/** MySQL hostname */
+define('DB_HOST', 'localhost:8889');
+
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8mb4');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY',         '..hhm_yP!U/z!$;X{c4}G}sh;p{_,z0z!/0&wHZ{+g/-=[CJ;6HN(w@3)lucJ%lU');
+define('SECURE_AUTH_KEY',  'H2&YBa o2LH#iT pOY}9Kb9:u*C&GIuJDkn`,[zo*6By2U~70&vf6v-?X8Mh|<B#');
+define('LOGGED_IN_KEY',    'GnOy]_3(D6V=d37&KdPEWN3 iX{tiVP)^Lf]@gca;*.kD#.qc@m aw/N|v6/&uoJ');
+define('NONCE_KEY',        ':4Rs[(t``/63L<d`Idi;=elu0/nK<C#R-}#s%/RB~q/y7}ap`/ZD#9~0q~5lKVgM');
+define('AUTH_SALT',        '*j.Z&4SAT75<g(!v,1Wk38c7/#Y&)~ ZJU1_cJgf1a0Ix^)AgOwY5Q7$5J.=58@u');
+define('SECURE_AUTH_SALT', '3&)9n(s@nzqr]5X$rjde|NwVwZZ#[r7bif[1(!M36kV]t3 XQh$=>kCGOme_>YUR');
+define('LOGGED_IN_SALT',   '!4W|yyq9@k1ou)wN*i|j83V+jP3g2_H|Ou_?e,99]h@{^[d,J]sKAa40g:-S$5ao');
+define('NONCE_SALT',       'X93B/cL-A*g{Q3TZ qZ{BhJ~LaaAi;d+;]Ngnp/]Q:-b~f@0xoS. /^:G@1q3c_l');
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix  = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the Codex.
+ *
+ * @link https://codex.wordpress.org/Debugging_in_WordPress
+ */
+define('WP_DEBUG', false);
+
+/* That's all, stop editing! Happy blogging. */
+
+/** Absolute path to the WordPress directory. */
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Sets up WordPress vars and included files. */
+require_once(ABSPATH . 'wp-settings.php');
---
+++ wp-content/themes/understrap-child/functions.php
@@ -0,0 +1,134 @@
+<?php
+/* constants */
+define('IMAGE_PATH','/wp-content/themes/understrap-child/images/');
+
+/* general site settings and clean up */
+if (!defined('WP_POST_REVISIONS')) define('WP_POST_REVISIONS', 5);
+
+function understrap_remove_scripts() {
+    wp_dequeue_style('google-fonts-1');
+
+    wp_dequeue_style( 'understrap-styles' );
+    wp_deregister_style( 'understrap-styles' );
+
+    wp_dequeue_script( 'understrap-scripts' );
+    wp_deregister_script( 'understrap-scripts' );
+
+    // Removes the parent themes stylesheet and scripts from inc/enqueue.php
+}
+add_action( 'wp_enqueue_scripts', 'understrap_remove_scripts', 20 );
+
+function theme_enqueue_styles() {
+
+	// Get the theme data
+	$the_theme = wp_get_theme();
+    wp_enqueue_style( 'child-understrap-styles', get_stylesheet_directory_uri() . '/css/child-theme.min.css', array(), $the_theme->get( 'Version' ) );
+    wp_enqueue_script( 'jquery');
+	  wp_enqueue_script( 'popper-scripts', get_template_directory_uri() . '/js/popper.min.js', array(), false);
+    wp_enqueue_script( 'child-understrap-scripts', get_stylesheet_directory_uri() . '/js/child-theme.min.js', array(), $the_theme->get( 'Version' ), true );
+    if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+        wp_enqueue_script( 'comment-reply' );
+    }
+
+    wp_enqueue_style( 'child-understrap-custom', get_stylesheet_directory_uri() . '/css/style.css', array(), $the_theme->get( 'Version' ) );
+}
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+
+
+// Add to head needed scripts
+function child_theme_head_script() {
+	?>
+  <script>
+  (function(d) {
+	var config = {
+		kitId: 'cvm5nhj',
+		scriptTimeout: 3000,
+		async: true
+	},
+	h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+  })(document);
+  </script>
+  <?php
+}
+add_action( 'wp_head', 'child_theme_head_script' );
+
+
+function get_header1(){
+	return '<nav class="navbar navbar-expand-lg navbar-dark">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <a class="navbar-brand" href="/"><img src="'.IMAGE_PATH.'ph-logo.png" width="180" alt="Pursuit Health Tech Logo" class="img-fluid"></a>
+
+
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto" id="header-nav">
+      <li class="nav-item">
+        <a class="nav-link text-white" href="/">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-white" href="/pricing">Pricing</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-white" href="/about">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-white" href="/contact-us">Contact</a>
+      </li>
+      <li class="nav-item">
+        <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Download</button>
+      </li>
+    </ul>
+      
+  </div>
+</nav>';
+}
+add_shortcode( 'header', 'get_header1' );
+
+function get_footer_1($atts){
+  return '<div class="container pt-5 pb-5 text-white">
+  <div class="row">
+  <div class="col"><a href="/"><img src="'.IMAGE_PATH.'ph-logo.png" width="150" alt="Pursuit Health Tech Logo" class="img-fluid"></a>
+  <br>
+  <span class="small"><span class="small">&copy; 2018 All rights reserved.</span> Phoenix, AZ</span></div>
+  <div class="col-4 col-md-3">
+  <ul class="list-unstyled text-center">
+  <li><a href="/" class="text-white small">Home</a></li>
+  <li><a href="/about" class="text-white small">About</a></li>
+  <li><a href="#" class="text-white small">FAQ</a></li>
+  </ul>
+  </div>
+  <div class="col-4 col-md-3">
+  <ul class="list-unstyled text-center">
+  <li><a href="/contact-us" class="text-white small">Contact</a></li>
+  <li><a href="/pricing" class="text-white small">Pricing</a></li>
+  </ul>
+  </div>
+  <div class="mt-3 mt-md-0 col-md-3 text-center text-white">
+  <p>Stay in Touch</p>
+  <div class="row mb-2">
+  <div class="col-6"><a href="https://www.facebook.com/PursuitHealth/" target="_blank"><img src="/wp-content/uploads/2018/05/facebook-icon@2x.png" alt="" class="float-right d-block mx-auto img-fluid" width="32" height="32"></a></div>
+  <div class="col-6"><a href="#" target="_blank"><img src="/wp-content/uploads/2018/05/ig-icon@2x.png" alt="" class="float-left d-block mx-auto img-fluid" width="32" height="32"></a></div>
+  </div>
+  <div class="small">
+  <a href="mailto:contact@pursuithealthtech.com" class="text-white">contact@pursuithealthtech.com</a><br>
+  <a href="tel:+1-602-759-0732" class="text-white">602-759-0732</a>
+  </div>
+  </div>
+  </div>
+  </div>';
+}
+
+add_shortcode('footer','get_footer_1');
+
+
+function load_page_scripts(){
+  if(is_page(150)){
+    echo '<link href="https://cdnjs.cloudflare.com/ajax/libs/rangeslider.js/2.3.2/rangeslider.min.css" rel="stylesheet">';
+    echo '<script src="https://cdnjs.cloudflare.com/ajax/libs/rangeslider.js/2.3.2/rangeslider.min.js"></script>';
+    echo "<script>jQuery('input[type=\"range\"]').rangeslider();</script>";
+  }
+}
+
+add_action('wp_footer','load_page_scripts',100);
