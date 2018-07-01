11da04c7c284098b495574272d2593e387b90161
Steve DiGirolamo <steve@arizonawebdevelopment.com>
2018-06-30 22:30:42 -0700

    current production

--- wp-content/themes/understrap-child/functions.php
+++ wp-content/themes/understrap-child/functions.php
@@ -65,7 +65,7 @@ function get_header1(){
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <a class="navbar-brand" href="/"><img src="'.IMAGE_PATH.'ph-logo.png" width="180" alt="Pursuit Health Tech Logo" class="img-fluid"></a>
+  <a class="navbar-brand" href="/"><img src="'.IMAGE_PATH.'ph-logo.png" width="220" alt="Pursuit Health Tech Logo" class="img-fluid"></a>
 
 
 
